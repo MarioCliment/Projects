4a2548b0eff8b849467495909293448e4f308429
Mario Climent Jiménez <mclijim@epsg.upv.es>
2023-04-13 11:50:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ const displayProfile = (profile) => {
         <div>
             <h2><a href=${profile.blog}><strong>${profile.name}</strong></a></h2>
             <p>${profile.bio}</p>
-            <p>
+            <!--p>
                 <strong>Location:</strong> ${profile.location}
-            </p>
+            </p-->
             <p>
                 <strong>@${profile.login} </strong>
                 Repos: ${profile.public_repos}
-                Gists: ${profile.public_gists}
+                <!--Gists: ${profile.public_gists}-->
             </p>
         </div>
     `;
